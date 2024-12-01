23a7a85dd5f3bed1f133f448d6f04604f6c8fde6
Mikuu <mikuu252@gmail.com>
2024-12-01 18:14:44 +0100

    Add insert_error insert_burst_error

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from decoder import Decoder
-from coder import Coder
-from galois import Galois
+from simulation import Simulation
 
 if __name__ == '__main__':
-    galois = Galois()
-    galois.generate_generative_polynomial()
+    sim = Simulation()
 
-    coder = Coder(galois)
-    decoder = Decoder(galois)
-
-    text = "Lorem ipsum dolor sit amet."
-    coded_text = coder.code(text)
-    decoded_text = decoder.decode(coded_text)
-
-    print(decoded_text)
-    print(f"{'Decoded succesfully' if text == decoded_text else 'Decoded with errors'}")
+    sim.start_simulation()
